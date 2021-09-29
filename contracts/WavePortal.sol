5c41663fd9efc33331877024ab8763d601fcb7fc
William Hunter <30416118+wlhunter00@users.noreply.github.com>
2021-09-29 01:48:18 -0500

    made more process

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    uint256 totalWaves;
+
+    constructor() {
+        console.log("This is Will's first smart contract.");
+    }
+
+    function wave() public {
+        totalWaves++;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
+    }
+}
