36e48d9ce4485b7588b0df3fd8c10262011b1329
William Hunter <30416118+wlhunter00@users.noreply.github.com>
2021-10-06 00:32:17 -0500

    storing messages

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -7,13 +7,31 @@ import "hardhat/console.sol";
 contract WavePortal {
     uint256 totalWaves;
 
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    struct Wave {
+        address waver;
+        string message;
+        uint256 timestamp;
+    }
+
+    Wave[] waves;
+
     constructor() {
         console.log("This is Will's first smart contract.");
     }
 
-    function wave() public {
+    function wave(string memory _message) public {
         totalWaves++;
         console.log("%s has waved!", msg.sender);
+
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
